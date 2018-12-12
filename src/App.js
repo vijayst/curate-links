11d028a12258463f7e12df6587f8f44db03286ca
vijayst <vijayst@gmail.com>
2018-12-12 19:41:24 +0530

    coming soon page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    Hello world
-                </header>
+            <div className="app">
+                <div className="title">Curate Links</div>
+                <div className="subtitle">Organize knowledge and help others</div>
+                <div className="soon">Coming Soon ...</div>
             </div>
         );
     }
