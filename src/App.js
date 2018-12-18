0521be254aa82aa4b94293009921bb51936e2b45
vijayst <vijayst@gmail.com>
2018-12-18 12:46:51 +0530

    google adsense

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ class App extends Component {
         return (
             <div className="app">
                 <div className="title">Curate Links</div>
-                <div className="subtitle">Organize knowledge and help others</div>
+                <div className="subtitle">
+                    Organize knowledge and help others
+                </div>
                 <div className="soon">Coming Soon ...</div>
+                <div className="ads">
+                    <ins
+                        className="adsbygoogle"
+                        style={{ display: 'block' }}
+                        data-ad-client="ca-pub-2746031862127149"
+                        data-ad-slot="7309827854"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"
+                    />
+                </div>
             </div>
         );
     }
