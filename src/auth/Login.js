03f1e5dc08656d6073b5dd28cf5073407a90ae43
vijayst <vijayst@gmail.com>
2018-12-19 20:53:56 +0530

    login page styles

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export default function Login() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <form>
-            <div className="title">Login</div>
-            <input type="text" className="textbox" placeholder="email" />
-            <input type="password" className="textbox" placeholder="password" />
-            <button className="button">Login</button>
-        </form>
+        <div className="login">
+            <form onSubmit={handleSubmit}>
+                <div className="title">Login</div>
+                <input type="text" className="textbox" placeholder="email" />
+                <input
+                    type="password"
+                    className="textbox"
+                    placeholder="password"
+                />
+                <div className="login__button">
+                    <button className="button" disabled>Login</button>
+                </div>
+            </form>
+        </div>
     );
 }
