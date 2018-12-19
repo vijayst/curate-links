3e5c77755be7ecfccec4ecbff0c7f3b19c4dd597
vijayst <vijayst@gmail.com>
2018-12-19 09:31:23 +0530

    remove ads

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    componentDidMount() {
-        if (window.adsbygoogle) {
-            window.adsbygoogle.push({});
-        }
-    }
-
     render() {
         return (
             <div className="app">
@@ -15,16 +9,6 @@ class App extends Component {
                     Organize knowledge and help others
                 </div>
                 <div className="soon">Coming Soon ...</div>
-                <div className="ads">
-                    <ins
-                        className="adsbygoogle"
-                        style={{ display: 'block' }}
-                        data-ad-client={process.env.REACT_APP_AD_CLIENT}
-                        data-ad-slot={process.env.REACT_APP_AD_SLOT}
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"
-                    />
-                </div>
             </div>
         );
     }
