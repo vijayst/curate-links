c0378374f8cc816ba847b91585a774ac9a7dceb5
vijayst <vijayst@gmail.com>
2018-12-20 11:14:05 +0530

    animate using react-move

--- src/common/Message.js
+++ src/common/Message.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Animate } from 'react-move';
+import PropTypes from 'prop-types';
 
 export default function Message(props) {
     useEffect(
@@ -15,9 +17,41 @@ export default function Message(props) {
         [props.text]
     );
 
-    return props.text ? (
-        <div className={props.error ? 'message message--error' : 'message'}>
-            {props.text}
-        </div>
-    ) : null;
+    return (
+        <Animate
+            show={!!props.text}
+            start={{
+                opacity: 0,
+                className: props.error ? 'message message--error' : 'message',
+                text: props.text
+            }}
+            enter={{
+                opacity: [1],
+                timing: {
+                    duration: 300
+                }
+            }}
+            leave={{
+                opacity: [0],
+                timing: {
+                    duration: 150
+                }
+            }}
+        >
+            {({ opacity, className, text }) => (
+                <div
+                    style={{ opacity }}
+                    className={className}
+                >
+                    {text}
+                </div>
+            )}
+        </Animate>
+    );
+}
+
+Message.propTypes = {
+    text: PropTypes.string,
+    error: PropTypes.bool,
+    onClose: PropTypes.func
 }
