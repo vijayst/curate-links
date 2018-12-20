6116dedeb64a8d8cabe8376f0a8806ea44529818
vijayst <vijayst@gmail.com>
2018-12-20 10:49:27 +0530

    firebase login error

--- src/common/Message.js
+++ src/common/Message.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Message(props) {
+    useEffect(
+        () => {
+            if (props.text) {
+                const timeout = setTimeout(() => {
+                    props.onClose();
+                }, 5000);
+                return () => {
+                    clearTimeout(timeout);
+                };
+            }
+        },
+        [props.text]
+    );
+
     return props.text ? (
         <div className={props.error ? 'message message--error' : 'message'}>
             {props.text}
