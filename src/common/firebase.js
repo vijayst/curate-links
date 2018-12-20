ebf11bc019674dcb30455ffac5519ea8f5759921
vijayst <vijayst@gmail.com>
2018-12-20 11:45:55 +0530

    remove firebase console error

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
     apiKey: "AIzaSyCiKuSouOr19IIR-GgRQyKo-HDzAQzVQ_E",
