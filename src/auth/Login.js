bad5e7366b4501868b3b17f04081cb45c95823bb
vijayst <vijayst@gmail.com>
2018-12-20 10:04:07 +0530

    enable button after email, password is entered

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [disabled, setDisabled] = useState(true);
+
     function handleSubmit(e) {
         e.preventDefault();
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+        setDisabled(!(e.target.value && password));
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+        setDisabled(!(e.target.value && email));
     }
 
     return (
         <div className="login">
             <form onSubmit={handleSubmit}>
                 <div className="title">Login</div>
-                <input type="text" className="textbox" placeholder="email" />
+                <input autofocus type="text" className="textbox" placeholder="email" onChange={handleEmailChange} />
                 <input
                     type="password"
                     className="textbox"
                     placeholder="password"
+                    onChange={handlePasswordChange}
                 />
                 <div className="login__button">
-                    <button className="button" disabled>Login</button>
+                    <button className="button" disabled={disabled}>Login</button>
                 </div>
             </form>
         </div>
