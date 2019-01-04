8a032b05d993c75a72b352fb68fd30fbec2d6e7a
vijayst <vijayst@gmail.com>
2019-01-04 20:25:43 +0530

    admin page layout

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Admin() {
     return (
-        <div>Admin</div>
+        <div className="admin">
+            <header className="admin__header">
+                Curate Links
+            </header>
+            <aside className="admin__nav">
+                <ul className="nav">
+                    <li className="nav__item"><Link to="/admin/topic/create">Create Topic</Link></li>
+                    <li className="nav__item"><Link to="/admin/settings">Settings</Link></li>
+                </ul>
+            </aside>
+            <main className="admin__content">
+
+            </main>
+            <footer className="admin__footer">
+                &copy;2019, All rights reserved
+            </footer>
+        </div>
     );
 }
