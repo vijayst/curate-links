598009cfeba11bf5184935c943e9fc7e53e996d7
vijayst <vijayst@gmail.com>
2019-01-20 09:57:20 +0530

    home page redesign

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <div className="home">
-            <div className="home__title">Curate Links</div>
-            <div className="home__subtitle">Organize knowledge and help others</div>
-            <div className="home__soon">Coming Soon ...</div>
+            <div className="home__inner">
+                <div className="home__inner__title">Curate Links</div>
+                <div className="home__inner__github">
+                    <a href="https://github.com/vijayst/curate-links">GitHub</a>
+                </div>
+                <div className="home__inner__demo">
+                    <Link to="/topics/react">React</Link>
+                </div>
+            </div>
         </div>
     );
 }
