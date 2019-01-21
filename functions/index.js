f3bec554562a010d20def8822e6b97ca3caaf619
vijayst <vijayst@gmail.com>
2019-01-21 16:19:43 +0530

    valid url

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,24 @@
 const functions = require('firebase-functions');
+const axios = require('axios');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send('Hello from Firebase!');
+exports.valid = functions.https.onRequest((request, response) => {
+    const url = request.query.url || request.body.url;
+    if (!url) {
+        response.status(400).send('URL is required');
+        return;
+    }
+    axios
+        .head(url)
+        .then(res => {
+            const iframe = !res.headers['x-frame-options'];
+            response
+                .status(200)
+                .send({
+                    valid: true,
+                    iframe
+                });
+        })
+        .catch(() => {
+            response.status(200).send({ valid: false });
+        });
 });
