f2ad9086f02121124c37059b1c105b04925ed5bd
vijayst <vijayst@gmail.com>
2019-01-21 17:31:59 +0530

    meta function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
+const urlMetadata = require('url-metadata');
 
 exports.valid = functions.https.onRequest((request, response) => {
     const url = request.query.url || request.body.url;
@@ -20,5 +21,29 @@ exports.valid = functions.https.onRequest((request, response) => {
         })
         .catch(() => {
             response.status(200).send({ valid: false });
+        });
+});
+
+exports.meta = functions.https.onRequest((request, response) => {
+    const url = request.query.url || request.body.url;
+    if (!url) {
+        response.status(400).send('URL is required');
+        return;
+    }
+    urlMetadata(url)
+        .then(metadata => {
+            let { title, image, description } = metadata;
+            image = image || metadata['og:image'];
+            description = description || metadata['og:description'];
+            response
+                .status(200)
+                .send({
+                   title,
+                   image,
+                   description
+                });
+        })
+        .catch(() => {
+            response.status(500).send('Unknown error');
         });
 });
