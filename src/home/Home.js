e0e89256fa48f5931c6d3e5ddfc6ffa9d8b88d4d
vijayst <vijayst@gmail.com>
2019-01-22 10:38:18 +0530

    fix circle in home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return (
         <div className="home">
-            <div className="home__inner">
-                <div className="home__inner__title">Curate Links</div>
-                <div className="home__inner__github">
-                    <a href="https://github.com/vijayst/curate-links">GitHub</a>
-                </div>
-                <div className="home__inner__demo">
-                    <Link to="/topics/react">React</Link>
+            <div className="home__circle">
+                <div className="home__inner">
+                    <div className="home__inner__title">Curate Links</div>
+                    <div className="home__inner__github">
+                        <a href="https://github.com/vijayst/curate-links">
+                            GitHub
+                        </a>
+                    </div>
+                    <div className="home__inner__demo">
+                        <Link to="/topics/react">React</Link>
+                    </div>
                 </div>
             </div>
         </div>
